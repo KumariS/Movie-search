9224e7b3548bdc3070b8305b05d19511c899f411
narencoderpad <narencoderpad@gmail.com>
2020-08-07 19:37:36 +0530

    movie search app code commits

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import VideoComponent from "./Component/VideoComponent"
+import './App.css';
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }  
+
+  
+  render() {
+    return (
+      <div className="">
+      <BrowserView>
+          <VideoComponent/>
+      </BrowserView>
+      <MobileView>
+      <VideoComponent MobileClass="100%" padClass={"Pad0"}/>
+      </MobileView>
+      </div>
+    );
+  }
+}
+
+export default App;
